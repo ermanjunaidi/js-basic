679292fa1e4b45c7ea1b85cb72f420160807223c
juna <ermanjunaidi1@gmail.com>
2019-01-04 16:56:14 +0700

    2- Arithmetic Operators

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-let x = (5 + 7 - 3) * 25
-console.log(x)
+let x = 20;
+let y = 6;
+let z = 15;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+
+//Increment
+console.log(x++);
+console.log(x);
+
+//Decrement
+
+console.log(z--);
+console.log(z);
