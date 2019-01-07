6855872a0ca2e341e2f6ebec5ebda8cb4012980a
juna <ermanjunaidi1@gmail.com>
2019-01-07 21:20:30 +0700

    9- Bitwise Operators

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-let userColor = undefined;
-let defaultColor = 'blue';
-let currentColor = userColor || defaultColor;
+const readPermissions = 4;
+const writePermission = 2;
+const executePermission = 1;
 
-console.log(currentColor);
+let myPermission = 5;
+myPermission = myPermission | writePermission
+
+let message = (myPermission & readPermissions) ?  'iya': 'tidak';
+console.log(message);
