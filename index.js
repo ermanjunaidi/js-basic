088c865c12dcab5a02dd7317804fb77b11cb6d3e
juna <ermanjunaidi1@gmail.com>
2019-01-08 20:51:59 +0700

    4- While Control Flow

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-for (let i=1; i <= 100; i++){
+for (let i=0; i <= 5; i++){
     if (i % 2 !== 0)
     console.log('Hello World', i)
+} 
+
+let i = 0
+while (i <= 10) {
+    if (i % 2 !== 0)
+i++    
 }
