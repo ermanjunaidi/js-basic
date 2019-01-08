794e4997cf9e968c3fe5b7812e45fdd49a47ad84
juna <ermanjunaidi1@gmail.com>
2019-01-08 22:10:18 +0700

    8- For...of Control Flow

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
-// const person = {
-//     nama: 'erman',
-//     umur: 30
-// };
+const person = {
+    nama: 'erman',
+    umur: 30
+};
 
-// for (let key in person)
+for (let key in person)
 
-// console.log(key, person[key])
+console.log(key, person[key])
 
 const colors = ['red', 'green', 'blue', 'yellow', 'purple']
 
-for(let index in colors)
+// for(let index in colors)
 
-console.log(index, colors[index])
+// console.log(index, colors[index])
+
+// For Of
+for (let color of colors)
+console.log(color);
