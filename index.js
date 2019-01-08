36ddfbbd58fb785c061e0b0daa034c4771b31b97
juna <ermanjunaidi1@gmail.com>
2019-01-08 22:24:10 +0700

    9- Break and Continue Control Flow

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-
-const person = {
-    nama: 'erman',
-    umur: 30
-};
-
-for (let key in person)
-
-console.log(key, person[key])
-
-const colors = ['red', 'green', 'blue', 'yellow', 'purple']
-
-// for(let index in colors)
-
-// console.log(index, colors[index])
-
-// For Of
-for (let color of colors)
-console.log(color);
+let i = 0
+while (i <= 100) {
+    if (i % 2 === 0) {
+        i++;
+        continue
+    }
+    
+    console.log(i)
+    i++
+}
