301ef7b8873cbe3f60bb80868ad01bd7257bd2cd
juna <ermanjunaidi1@gmail.com>
2019-01-08 21:27:41 +0700

    5- Do...while Control Flow

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-for (let i=0; i <= 5; i++){
-    if (i % 2 !== 0)
-    console.log('Hello World', i)
-} 
+// for (let i = 0; i <= 5; i++) {
+//     if (i % 2 !== 0) console.log(i)
+// }
 
-let i = 0
-while (i <= 10) {
-    if (i % 2 !== 0)
-i++    
-}
+// let i = 0
+// while (i <= 5) {
+//     if (i % 2 !== 0) console.log(i) 
+//     i++
+// }
+
+// Do While
+let i = 13;
+do {
+
+    if (i % 2 !== 0) console.log(i) 
+    i++;
+} while (i <= 5);
