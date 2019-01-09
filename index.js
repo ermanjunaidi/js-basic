58b984b3392597bba45fb79286355b7cc70d2964
juna <ermanjunaidi1@gmail.com>
2019-01-09 13:42:49 +0700

    8- For...of Control Flow

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-console.log(isLandscape(800, 700))
-function isLandscape (width, height) {
-    return (width > height)
+const output = fizzbuzz(1)
+console.log(output)
+
+function fizzbuzz(input) {
+    if(typeof input !== 'number')
+        return NaN
+        
+    if ((input % 3 === 0 ) && (input % 5 === 0))
+        return 'fizz Buzz'
+
+    if (input % 3 === 0)
+        return 'fizz'
+
+    if (input % 5 === 0)
+        return 'buzz'
+        
+    
+        return input
 }
