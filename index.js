c4a1a220fb1dd256d6a1caa96a5e02be68fe1565
juna <ermanjunaidi1@gmail.com>
2019-01-12 05:43:44 +0700

    20- Exercise- Prime Numbers Control Flow

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-showStars(10)
-function showStars(rows) {
-    for (let row = 1; row <= rows; row++) {
-        let pattern = ''
-        for (let i = 0; i < row; i++)
-        pattern += '*'
-        console.log(pattern)
+showPrimes(50)
+
+
+function showPrimes(limit) {
+    for (let number = 2; number <= limit; number++)    
+        if (isPrime(number)) console.log(number)
     }
+    
+    function isPrime(number) {   
+        for (let factor = 2; factor < number; factor++) 
+        if (number % factor === 0) 
+            return false
+            return true
+        
 }
