c6d2f5dd42ac7078b531107e4fc5dcc183205210
juna <ermanjunaidi1@gmail.com>
2019-01-11 08:06:22 +0700

    14- Exercise 5- Even and Odd Numbers Control Flow

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
+showNumbers(20)
 
-checkSpeed(120)
-function checkSpeed(speed) {
-    
-    const speedLimit = 70
-    const kmPerPoint = 5
-    
-    if (speed < speedLimit + kmPerPoint) {
-        console.log('ok')
-        return
-        }
-    else {
-        const points = Math.floor((speed - speedLimit) / kmPerPoint)
-        if (points >= 12)
-        console.log('License Suspended')
-        else
-        console.log('Points', points)
+function showNumbers(limit) {
+    for (let i=0; i <= limit; i++) {
+        // if (i % 2 === 0) console.log(i, 'Event')
+        // else console.log(i, 'ODD')
 
+        const message = (i % 2 === 0) ? 'Erman' : 'Juna'
+        console.log(i, message)
     }
-
 }
