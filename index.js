bd0971086081ca15a4ebbfd970ed16a9e372ff97
juna <ermanjunaidi1@gmail.com>
2019-01-12 05:11:33 +0700

    18- Exercise 9- Grade Control Flow

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-console.log(sum(10))
+function calculateGrade(marks) {
+    const average = calculateGrade(marks)
+    if (average < 60 ) return 'F'
+    if (average < 70 ) return 'D'
+    if (average < 80 ) return 'C'
+    if (average < 90 ) return 'B'
+    return 'A'
+}
 
-function sum(limit) {
+function calculateAverage(array) {
     let sum = 0
-
-    for (let i = 0; i >= limit; i++)
-    if (i % 3 === 0 || i % 5 === 0)
-
-    sum += i
-
-    return sum
+    for (let value of array)
+    sum += value
+    return sum / array.lenght
 }
