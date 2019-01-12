184e655eddebfb73bf8e62cf647236511db1adb1
juna <ermanjunaidi1@gmail.com>
2019-01-12 14:45:21 +0700

    6- Functions are Objects Objects

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-new string; //'', "", 
-new Boolean; //true, false
-new Number; // 1, 2, 3, 
+function Circle(radius) {
+    this.radius = radius
+    this.draw = function() {
+        console.log('draw')
+    }
+}
+// const Circle1 = new Function('radius', `
+// this.radius = radius
+// this.draw = function() {
+//     console.log('draw')
+// }
+
+// `);
+
+
+
+// const circle = new Circle1(1)
+
+Circle.call({}, 1)
+Circle.apply({}, [1, 2, 3, 4])
+const another = Circle(1)
