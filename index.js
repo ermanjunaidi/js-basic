6f85518dc1a0842b20ef7fcb986fd1fc773f9185
juna <ermanjunaidi1@gmail.com>
2019-01-12 13:37:56 +0700

    5- Constructor Property Objects

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const circle = {
-    radius: 1
-}
-
-circle.color = 'yellow'
-circle.draw = function() {}
-
-delete circle.color
-delete circle.draw
-
-console.log(circle)
+new string; //'', "", 
+new Boolean; //true, false
+new Number; // 1, 2, 3, 
